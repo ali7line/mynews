e256867170923673daff3628c37d345271b95855
ali7line <soltani.110@gmail.com>
2017-04-21 11:55:01 +0430

    adds reddit programming page

--- mynews/spiders/red_news.py
+++ mynews/spiders/red_news.py
@@ -29,33 +29,23 @@ class SomethingSpider(CrawlSpider):
         @returns items 25
         @scrapes news_id
         """
-        selector_list = response.css('div.thing')
-                
-        for selector in selector_list:
-            item = MyyItem()
-            #item['image_urls'] = selector.xpath('a[contains(@class, "thumbnail")]/@href').extract()
-            item['title'] = selector.xpath('div/p/a/text()').extract()
-            item['link'] = selector.xpath('a/@href').extract()
-                    
-            yield item
+        id_list = response.xpath('//div[contains(@class, "thing")]/@id').extract()
+        for id_ in id_list:
+            l = ItemLoader(item=MyyItem(), response=response)
+            l.add_xpath('news_id', id_)
+            xpath_header = '//div[@id="{}]"'.format(id_)
+            l.add_xpath('title', xpath_header + '//p[@class="title"]/a/text()')
+            l.add_xpath('site', xpath_header + '//span[@class="domain"]/a/text()')
+            l.add_xpath('link', xpath_header + '//p[@class="title"]/a/@href')
+            l.add_xpath('points', xpath_header + '//div[contains(@class, "score") and contains(@class, "unvoted")]/text()', re='[0-9]+')
+            l.add_xpath('rank', xpath_header + '//span[@class="rank"]/text()', re='[0-9]+')
+            l.add_xpath('author', xpath_header + '//a[contains(@class, "author")]/text()')
+            l.add_xpath('comments', xpath_header + '//a[contains(@class, "comments")]', re='[0-9]+')
+            l.add_xpath('age', xpath_header + '//time/@datetime')
 
-        #page_id = response.xpath('//tr[@class="athing"]/@id').extract()
-        #for news_id in page_id:
-        #    l = ItemLoader(item=MyyItem(), response=response)
-        #    l.add_xpath('news_id', news_id)
-        #    xpath_header = '//*[@id="{}"]'.format(news_id)
-        #    l.add_xpath('title', xpath_header + '/td[3]/a/text()')
-        #    l.add_xpath('site', xpath_header + '//span[@class="sitestr"]/text()')
-        #    l.add_xpath('link', xpath_header + '//a[@class="storylink"]/@href')
-        #    l.add_xpath('author', xpath_header + '/following-sibling::tr[1]/td[@class="subtext"]/a[1]/text()')
-        #    l.add_xpath('points', '//*[@id="score_{}"]/text()'.format(news_id), re='[0-9]+')
-        #    l.add_xpath('rank', xpath_header + '/td[1]/span/text()', re='[0-9]+')
-        #    l.add_xpath('comments', xpath_header + '/following-sibling::tr[1]/td[@class="subtext"]/a[3]/text()', re='[0-9]+')
-        #    l.add_xpath('age', xpath_header + '/following-sibling::tr[1]//span[@class="age"]/a/text()')
-
-        #    l.add_value('url',response.url)
-        #    l.add_value('server',socket.gethostname())
-        #    l.add_value('project',self.settings.get('BOT_NAME'))
-        #    l.add_value('spider',self.name)
-        #    l.add_value('date', datetime.datetime.now())
-        #    yield l.load_item()
+            l.add_value('url',response.url)
+            l.add_value('server',socket.gethostname())
+            l.add_value('project',self.settings.get('BOT_NAME'))
+            l.add_value('spider',self.name)
+            l.add_value('date', datetime.datetime.now())
+            yield l.load_item()
